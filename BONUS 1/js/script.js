f8cc7cfdcc03cc400fb4b486319e9b81be2f458c
Marco Braccini <marco.braccini@live.it>
2022-06-23 14:51:23 +0200

    End project BONUS 1 - Add fx hamburger supersayan "Just click play, enjoy!"

--- BONUS 1/js/script.js
+++ BONUS 1/js/script.js
@@ -13,18 +13,22 @@ function playCoachella() {
     var txt = document.getElementById("mascotte-txt-container");
         txt.classList.remove("mascotte-txt-container-2");
         txt.classList.add("mascotte-txt-container-1");
+    var supersayan = document.getElementById("goku");
+    var stick = document.getElementById("stick");
+    var mcdonalds = document.getElementById("hamburger");
     let soundbar = document.querySelectorAll(".sfx-1-l, .sfx-1-r, .sfx-2-l, .sfx-2-r, .sfx-3-l, .sfx-3-r, .sfx-4-l, .sfx-4-r");
         soundbar.forEach(soundbar => {
         console.log(soundbar);
         soundbar.style.animationPlayState = "running";})
-    if (play.style.display = "block") {
         play.style.display = "none";
         pause.style.display = "block";
         vinyl.style.animationPlayState = "running";
         current.style.animationPlayState = "running";  
         mascotte.style.animationPlayState = "running";  
-        txt.style.animationPlayState = "running";      
-    }
+        txt.style.animationPlayState = "running";
+        supersayan.style.display = "block";
+        stick.style.animationPlayState = "running";
+        mcdonalds.style.animationPlayState = "running";      
 }
 
 function pauseCoachella() {
@@ -38,16 +42,20 @@ function pauseCoachella() {
     var txt = document.getElementById("mascotte-txt-container");
         txt.classList.remove("mascotte-txt-container-1");
         txt.classList.add("mascotte-txt-container-2");
+    var supersayan = document.getElementById("goku");
+    var stick = document.getElementById("stick");
+    var mcdonalds = document.getElementById("hamburger");
     let soundbar = document.querySelectorAll(".sfx-1-l, .sfx-1-r, .sfx-2-l, .sfx-2-r, .sfx-3-l, .sfx-3-r, .sfx-4-l, .sfx-4-r");
         soundbar.forEach(soundbar => {
         console.log(soundbar);
         soundbar.style.animationPlayState = "paused";})
-    if (pause.style.display = "block") {
         pause.style.display = "none";
         play.style.display = "block";
         vinyl.style.animationPlayState = "paused";
         current.style.animationPlayState = "paused";
         mascotte.style.animationPlayState = "running";  
         txt.style.animationPlayState = "running";
-    }
+        supersayan.style.display = "none";
+        stick.style.animationPlayState = "paused";
+        mcdonalds.style.animationPlayState = "paused"; 
 }
